036e9e93f5254351d91bc13d3b6a6780b673530a
chie <ch1eawa@163.com>
2026-04-30 12:46:46 +0800

    加上关闭后记录session功能
    添加auto-session插件，自动保存session，用于再次打开时回到关闭前的状态

---
+++ lua/plugins/auto-session.lua
@@ -0,0 +1,22 @@
+return {
+	"rmagatti/auto-session",
+	lazy = false,
+	opts = {
+		auto_save = true,
+		auto_restore = true,
+		auto_create = true,
+
+		-- 避免在home\根目录\下载目录这类泛目录里乱存session
+		suppressed_dirs = {
+			"~/",
+			"~/Downloads/",
+			"/",
+		},
+
+		bypass_save_filetype = {
+			"alpha",
+			"dashboard",
+			"sancks_dashboard",
+		},
+	},
+}
